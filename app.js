593408c5de1775c4803d3c9236be62f0dfaaf3b7
andreschoco <andreschoco@gmail.com>
2022-09-03 05:16:46 -0500

    ultima version, todo funciona OK

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-const publipath = path.resolve(__dirname, './public');
-
-app.use(express.static(publipath ));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(3000, () =>{
     console.log('Servidor ok en puerto 3000');
 });
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+
 
